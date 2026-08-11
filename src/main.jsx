d596009dd752e2aaf703e3383d8f54f01e8668aa
naoboco <naomiebocobza@gmail.com>
2026-08-11 11:49:30 +0300

    Load final DAVIDEL demo polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles/global.css'
 import './styles/desktop-fixes.css'
 import './styles/splash.css'
+import './styles/demo-polish.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
